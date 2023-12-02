8b79039dcde524e04ba727ddc778a9c8a8066202
Jason S <jstodmdd@amazon.com>
2023-12-02 12:50:50 -0500

    removed files added gitignore

--- day-1/target/debug/day-1.d
+++
@@ -1,1 +0,0 @@
-/Users/jstodmdd/projects/aoc/advent-of-code-2023/day-1/target/debug/day-1: /Users/jstodmdd/projects/aoc/advent-of-code-2023/day-1/src/main.rs
--- day-1/target/debug/deps/day_1-9526f1758913865d.d
+++
@@ -1,5 +0,0 @@
-/Users/jstodmdd/projects/aoc/advent-of-code-2023/day-1/target/debug/deps/libday_1-9526f1758913865d.rmeta: src/main.rs
-
-/Users/jstodmdd/projects/aoc/advent-of-code-2023/day-1/target/debug/deps/day_1-9526f1758913865d.d: src/main.rs
-
-src/main.rs:
