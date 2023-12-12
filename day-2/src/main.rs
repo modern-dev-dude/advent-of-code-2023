06878c6fd9b1c9e1a651d98f286c5374e34421a6
Jason S <jstodmdd@amazon.com>
2023-12-12 00:56:59 -0500

    added day two part two

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -2,7 +2,7 @@ use regex::Regex;
 use std::collections::HashMap;
 use std::fs;
 
-fn main() {
+fn pt_one() {
     let file_path = "./input.txt";
     let file = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let contents: Vec<&str> = file.split("\n").collect();
@@ -104,5 +104,86 @@ fn main() {
         };
     }
 
-    println!("Total: {}", total.to_string());
+    println!("Part one solution {}", total.to_string());
+}
+
+fn pt_two() {
+    let file_path = "./input.txt";
+    let file = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents: Vec<&str> = file.split("\n").collect();
+
+    //12 red cubes, 13 green cubes, and 14 blue cubes
+    let mut cubes: HashMap<&str, i16> = HashMap::new();
+    cubes.insert("red", 0);
+    cubes.insert("green", 0);
+    cubes.insert("blue", 0);
+
+    let mut total: i32 = 0;
+    let split_pattern = " ";
+    for game in contents.iter() {
+        let red_re: Regex = Regex::new(r"([0-9]+ red)").unwrap();
+        let rc_captures = red_re.captures_iter(game);
+
+        for rc in rc_captures {
+            let rc_match = rc.get(1).map_or("0", |m| m.as_str());
+            let rc_num: i16 = rc_match.rsplit(split_pattern).collect::<Vec<_>>()[1]
+                .parse()
+                .unwrap();
+
+            if let Some(&value) = cubes.get("red") {
+                let new_rc_val = value.max(rc_num);
+                cubes.insert("red", new_rc_val);
+            }
+        }
+
+        // blue
+        let blue_re = Regex::new(r"([0-9]+ blue)").unwrap();
+        let bc_captures = blue_re.captures_iter(game);
+
+        for bc in bc_captures {
+            let bc_match = bc.get(1).map_or("0", |m| m.as_str());
+            let bc_num: i16 = bc_match.rsplit(split_pattern).collect::<Vec<_>>()[1]
+                .parse()
+                .unwrap();
+            if let Some(&value) = cubes.get("blue") {
+                let new_bc_val = value.max(bc_num);
+                cubes.insert("blue", new_bc_val);
+            }
+        }
+
+        // green
+        let green_re = Regex::new(r"([0-9]+ green)").unwrap();
+        let gc_captures = green_re.captures_iter(game);
+
+        for gc in gc_captures {
+            let gc_match = gc.get(1).map_or("0", |m| m.as_str());
+            let gc_num: i16 = gc_match.rsplit(split_pattern).collect::<Vec<&str>>()[1]
+                .parse()
+                .unwrap();
+
+            if let Some(&value) = cubes.get("green") {
+                let new_gc_val = value.max(gc_num);
+                cubes.insert("green", new_gc_val);
+            }
+        }
+
+        let rc_cube = unsafe { cubes.get("red").unwrap_unchecked() };
+        let gc_cube = unsafe { cubes.get("green").unwrap_unchecked() };
+        let blue_cube = unsafe { cubes.get("blue").unwrap_unchecked() };
+
+        let power_of_cubes = rc_cube * gc_cube * blue_cube;
+        total += power_of_cubes as i32;
+
+        // reset cubes lookup
+        cubes.insert("red", 0);
+        cubes.insert("green", 0);
+        cubes.insert("blue", 0);
+    }
+
+    println!("Part two solution {}", total.to_string());
+}
+
+fn main() {
+    pt_one();
+    pt_two();
 }
